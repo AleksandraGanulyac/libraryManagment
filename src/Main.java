a8d55e0e6fa2283a4d06fb2c4329e7c759285e44
AleksandraGanulyac <ganulya.aleksandra@gmail.com>
2026-09-15 16:52:40 +0300

    Легкий интерфейс

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,26 @@
-public class Main{
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Привет! Вы находитесь в библиотеке, ведите себя пожалуйста тихо");
+        System.out.println("Выберите и введите цифру");
+        System.out.println("1.Добавить книгу");
+        System.out.println("2.Добавить пользователя");
+        System.out.println("3.Посмотреть книги");
+        System.out.println("4.Выдача книги");
+        System.out.println("5.Возврат книги");
+        System.out.println("6.Просмотр Штрафов");
+        System.out.println("7.Отчеты");
+        System.out.println("8.Выход");
+        System.out.print("Выберите и введите цифру, соответствующую нужной информации: ");
+        choice = scanner.nextInt();
+
+        switch(choice){
+            case 1:
+
+        }
+    }
 }
