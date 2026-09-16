6c3d9889b861faba532ea8ac12d6c1575db0b9fd
AleksandraGanulyac <ganulya.aleksandra@gmail.com>
2026-09-16 22:43:44 +0300

    Просто поменяла название

--- src/Literature.java
+++ src/Literature.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
 
 public class Literature {
-    static record Book(int id, String type, String name, String author, int year){};
-    public static ArrayList<Book> books = new ArrayList<>();
+    static record Literaturs(int id, String type, String name, String author, int year){};
+    public static ArrayList<Literaturs> books = new ArrayList<>();
 
     public static void add(int id, String type, String name, String author, int year){
-        books.add(new Book(id, type, name, author, year));
+        books.add(new Literaturs(id, type, name, author, year));
     }
 
     public static void output(){
-        for(Book book: books){
+        for(Literaturs book: books){
             System.out.println("id: " + book.id() + "|Тип: " + book.type() + "|Название: " + book.name() + "|Автор: " + book.author() + "|Год написания: " + book.year());
         }
     }
