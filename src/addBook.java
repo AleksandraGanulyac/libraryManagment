5e5a2dae0983ecfc1b7df3a7235d69fe90e8a3e9
AleksandraGanulyac <ganulya.aleksandra@gmail.com>
2026-09-19 19:24:52 +0300

    теперь моно добавить несколько книг и несколько пользователей за раз не возвращаясь в меню

--- src/addBook.java
+++ src/addBook.java
@@ -11,7 +11,7 @@ public class addBook {
     public String name;
     public String author;
     public int year;
-    public String type;
+
     private static int id = 1;
     public boolean choice = true;
 
@@ -24,52 +24,73 @@ public class addBook {
     }
 
     public void addBook(){
-            System.out.println("Введите тип(книга, журнал, газета): ");
-            type = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
-            if (!type.isEmpty()){
-                type = type.substring(0,1).toUpperCase() + type.substring(1);
-            }
-            //Реализовать добавление в нужный массив в зависимости от выбора
+        System.out.println("Введите тип(книга, журнал, газета): ");
+        String type = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
+        if (!type.isEmpty()){
+            type = type.substring(0,1).toUpperCase() + type.substring(1);
+        }
+        //Реализовать добавление в нужный массив в зависимости от выбора
 
-            System.out.println("Введите название: ");
-            name = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
-            if (!name.isEmpty()){
-                name = name.substring(0,1).toUpperCase() + name.substring(1);
-            }
+        System.out.println("Введите название: ");
+        name = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
+        if (!name.isEmpty()){
+            name = name.substring(0,1).toUpperCase() + name.substring(1);
+        }
 
-            System.out.println("Введите автора: ");
-            author = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
-            if (!author.isEmpty()){
-                author = author.substring(0,1).toUpperCase() + author.substring(1);
-            }
+        System.out.println("Введите автора: ");
+        author = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
+        if (!author.isEmpty()){
+            author = author.substring(0,1).toUpperCase() + author.substring(1);
+        }
 
-            System.out.println("Введите год издания: ");
-            year = scanner.nextInt(); // какая нибудь проверка на число чтобы не вылетало с ошибкой
+        System.out.println("Введите год издания: ");
+        year = scanner.nextInt(); // какая нибудь проверка на число чтобы не вылетало с ошибкой
+        scanner.nextLine();
 
             //реализовать вывод с ID|Тип|Название|Автор|Год издания|На сколько можно взять(в зависимости от типа)
             //реализовать id с автоматическим увелечением
             //реализовать добавление в ArrayList
-            if(type.equals("Книга")){
-                book.add(getId(), name, author, year);
-            } else if (type.equals("Журнал")) {
-                magazine.add(getId(), name, author, year);
-            } else if (type.equals("Газета")) {
-                newspapper.add(getId(), name, author, year);
-            }
-            else{
-                System.out.println("Вы неверно ввели тип попробуйте снова");
-                Exit.exit();
-            }
-            Literature.add(getId(), type,  name, author, year);
-            increment();
-
+        if(type.equals("Книга")){
+            book.add(getId(), name, author, year);
+        } else if (type.equals("Журнал")) {
+            magazine.add(getId(), name, author, year);
+        } else if (type.equals("Газета")) {
+            newspapper.add(getId(), name, author, year);
+        }
+        else{
+            System.out.println("Вы неверно ввели тип попробуйте снова");
             Exit.exit();
+        }
+        Literature.add(getId(), type,  name, author, year);
 
-            //Вывод введенной информации перед возвратом в меню
-            //если нет, то начинается добавление еще одной книги
+        System.out.println("Вы добавили: ");
+        System.out.printf("ID: " + getId() + " | Тип: %s | Название: %s | Автор: %s | Год написаниея: %d" , type, name, author, year);
+        increment();
 
+        Continue();
+        //Вывод введенной информации перед возвратом в меню
+        //если нет, то начинается добавление еще одной книги
+
+    }
+    public void Continue(){
+        System.out.println("\nХотите ввести еще книгу(1-да, 2-нет): ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        switch(choice){
+            case 1:
+                addBook();
+                break;
+            case 2:
+                Exit.exit();
+                break;
+            default:
+                System.out.println("Неверная цифра попробуйте снвоа");
+                Continue();
+                break;
+        }
         scanner.close();
     }
+
 }
 
 
--- src/addUser.java
+++ src/addUser.java
@@ -23,48 +23,68 @@ public class addUser {
     }
 
     public void addUser(){
-        while(choice){
-            System.out.println("Введите тип(студент, преподаватель): ");
-            type = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
-            if (!type.isEmpty()){
-                type = type.substring(0,1).toUpperCase() + type.substring(1);
-            }
-            //Реализовать добавление в нужный массив в зависимости от выбора
+        System.out.println("Введите тип(студент, преподаватель): ");
+        type = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
+        if (!type.isEmpty()){
+            type = type.substring(0,1).toUpperCase() + type.substring(1);
+        }
+        //Реализовать добавление в нужный массив в зависимости от выбора
 
-            System.out.println("Введите Имя: ");
-            name = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
-            if (!name.isEmpty()){
-                name = name.substring(0,1).toUpperCase() + name.substring(1);
-            }
+        System.out.println("Введите Имя: ");
+        name = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
+        if (!name.isEmpty()){
+            name = name.substring(0,1).toUpperCase() + name.substring(1);
+        }
 
-            System.out.println("Введите Фамилию: ");
-            surname = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
-            if (!surname.isEmpty()){
-                surname = surname.substring(0,1).toUpperCase() + surname.substring(1);
-            }
+        System.out.println("Введите Фамилию: ");
+        surname = scanner.nextLine();// какая нибудь проверка на текст чтобы не вылетало с ошибкой
+        if (!surname.isEmpty()){
+            surname = surname.substring(0,1).toUpperCase() + surname.substring(1);
+        }
 
-            System.out.println("Введите год рождения: ");
-            year = scanner.nextInt(); // какая нибудь проверка на число чтобы не вылетало с ошибкой
+        System.out.println("Введите год рождения: ");
+        year = scanner.nextInt(); // какая нибудь проверка на число чтобы не вылетало с ошибкой
+        scanner.nextLine();
 
             //реализовать вывод с ID|Тип|Название|Автор|Год издания|На сколько можно взять(в зависимости от типа)
             //реализовать id с автоматическим увелечением
             //реализовать добавление в ArrayList
-            if(type.equals("Студент")){
-                student.add(getId(), name, surname, year);
-            } else if (type.equals("Преподаватель")) {
-                student.add(getId(), name, surname, year);
-            }
-            else{
-                System.out.println("Вы неверно ввели тип попробуйте снова");
-                Exit.exit();
-            }
-            User.add(getId(),  name,  surname, type, year);
-            increment();
+        if(type.equals("Студент")){
+            student.add(getId(), name, surname, year);
+        } else if (type.equals("Преподаватель")) {
+            student.add(getId(), name, surname, year);
+        }
+        else{
+            System.out.println("Вы неверно ввели тип попробуйте снова");
+            Continue();
+        }
+        User.add(getId(),  name,  surname, type, year);
 
-            Exit.exit();
+        System.out.println("Вы добавили: ");
+        System.out.printf("ID: " + getId() + " | Имя: %s | Фамилия: %s | Тип(преподаватель, студент): %s | Год рождения: %s", name, surname, type, year);
+        increment();
+
+        Continue();
+
+        scanner.close();
+    }
+    public void Continue(){
+        System.out.println("\nХотите ввести еще пользователя(1-да, 2-нет): ");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        switch(choice){
+            case 1:
+                addUser();
+                break;
+            case 2:
+                Exit.exit();
+                break;
+            default:
+                System.out.println("Неверная цифра попробуйте снвоа");
+                Continue();
+                break;
         }
         scanner.close();
     }
-
 
 }
